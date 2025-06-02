6978568e49c75866effb173cd02f4488ad9f2c68
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-02 16:02:40 -0400

    Add initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { type Game } from './game.ts'
 import { generateInitialGame, move } from './game.ts'
 
+
 type CellProps = {
   game: Game
   x: number
@@ -9,25 +10,25 @@ type CellProps = {
 }
 function CellDisplay({game, x, y}: CellProps) {
   if (game.board[y][x] === null) {
-    return `${x}, ${y}`
+    return null
   } else {
     return game.board[y][x]
-  }
-}
+  }}
 
 function App() {
   const [game, setGame] = useState(generateInitialGame())
   console.log(game)
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h1>Tic Tac Toe Mo</h1>
-      <div>
+      <div className="grid grid-cols-4 gap-2 max-w-fit">
         {
           game.board.map((row, rowIndex) =>
             row.map((cell, cellIndex) =>
               <div 
                 key={`${cellIndex}, ${rowIndex}`}
                 onClick={() => setGame(move(game, cellIndex, rowIndex))}
+                className="flex outline aspect-square w-16 justify-center items-center"
               > 
                 <CellDisplay game={game} x={cellIndex} y={rowIndex} />
               </div>
