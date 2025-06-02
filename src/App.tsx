024759d07cc735603f1cd459476eee4ad6368cb5
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-02 14:13:30 -0400

    Implement basic unstyled clickable board

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { useState } from 'react'
 import { Cell, Board, Player, Game } from './game.ts'
-import generateInitialGame from './game.ts'
+import { generateInitialGame, move } from './game.ts'
+
+type CellProps = {
+  game: Game
+  x: number
+  y: number
+}
+function Cell({game, x, y}: CellProps) {
+  if (game.board[y][x] === null) {
+    return 'BLANK'
+  } else {
+    return game.board[y][x]
+  }
+}
 
 function App() {
   const [game, setGame] = useState(generateInitialGame())
@@ -9,7 +22,17 @@ function App() {
     <div>
       <h1>Tic Tac Toe Mo</h1>
       <div>
-        {game.board.map(row => <div>{JSON.stringify(row)}</div>)}
+        {
+          game.board.map((row, rowIndex) =>
+            row.map((cell, cellIndex) =>
+              <div 
+                key={`${cellIndex}, ${rowIndex}`}
+                onClick={() => setGame(move(game, cellIndex, rowIndex))}
+              > 
+                <Cell game={game} x={cellIndex} y={rowIndex} />
+              </div>
+            ))
+        }
       </div>
       <p>
         It's {game.currentPlayer}'s turn!
