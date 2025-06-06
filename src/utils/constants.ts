26e5a5d1150ee8b314ef9222b81881457ee137fe
ajroberts0417 <ajroberts0417@gmail.com>
2025-06-06 15:28:53 -0400

    update prod server and client

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
-const ENVIRONMENT = (process.env.ENVIRONMENT || "dev") as "dev" | "production"
-const isProd = ENVIRONMENT === "production"
-export const SERVER_URL = isProd ? 'https://tictactoemo-deploy.onrender.com' : 'http://localhost:3000'
-export const CLIENT_URL = isProd ? 'https://incandescent-sundae-aa927a.netlify.app' : 'http://localhost:5173'
+export const SERVER_URL = 'https://tictactoemo-deploy.onrender.com'
+// export const SERVER_URL = 'http://localhost:3000'
+
+export const CLIENT_URL = 'https://incandescent-sundae-aa927a.netlify.app'
+// export const CLIENT_URL = 'http://localhost:5173'
