d60757f34cd2d5e84b33b1bb7b18ba2c503c42d2
ajroberts0417 <ajroberts0417@gmail.com>
2025-06-06 15:22:15 -0400

    update urls again

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 const ENVIRONMENT = (process.env.ENVIRONMENT || "dev") as "dev" | "production"
 const isProd = ENVIRONMENT === "production"
-export const SERVER_URL = isProd ? 'https://tictactoemo-deploy.onrender.com/' : 'http://localhost:3000'
-export const CLIENT_URL = isProd ? 'https://tictactoemo-deploy-6iw0vb2wc-ajroberts0417s-projects.vercel.app/' : 'http://localhost:5173'
+export const SERVER_URL = isProd ? 'https://tictactoemo-deploy.onrender.com' : 'http://localhost:3000'
+export const CLIENT_URL = isProd ? 'https://tictactoemo-deploy-krodmd3cb-ajroberts0417s-projects.vercel.app' : 'http://localhost:5173'
